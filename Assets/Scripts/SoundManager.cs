61ffe1d8768b35dd008d916729dcf0eb8f914db9
Owen1Campbell <ocampbell@knights.ucf.edu>
2022-02-20 18:58:45 -0500

    Animation commit

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,48 +4,94 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioClip musicClipOne;
+    
 
-    public AudioClip musicClipTwo;
+public AudioClip musicClipOne;
 
-    public AudioSource musicSource;
+public AudioClip musicClipTwo;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
+public AudioSource musicSource;
+
+Animator anim;
+
+void Start()
+
+{
+
+  anim = GetComponent<Animator>();
+
+}
+
+// Update is called once per frame
+
+void Update()
+
+{
+
+     if (Input.GetKeyDown(KeyCode.W))
+
         {
-            musicSource.clip = musicClipOne;
-            musicSource.Play();
+
+          musicSource.clip = musicClipOne;
+
+          musicSource.Play();
+
+          anim.SetInteger("State", 1);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.W))
+
+        {
+
+          musicSource.Stop();
+
+          anim.SetInteger("State", 0);
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.R))
+
+        {
+
+          musicSource.clip = musicClipTwo;
+
+          musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.R))
+
+        {
+
+          musicSource.Stop();
+
+          anim.SetInteger("State", 0);
+
+         }
+
+     if (Input.GetKeyDown(KeyCode.L))
+
+        {
+
+          musicSource.loop = true;
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+
+        {
+
+          musicSource.loop = false;
 
         }
 
-        if (Input.GetKeyUp(KeyCode.W))
+    if (Input.GetKeyDown("escape"))
         {
-            musicSource.Stop();
-
+            Application.Quit();
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            musicSource.clip = musicClipTwo;
-            musicSource.Play();
-        }
-
-        if (Input.GetKeyUp(KeyCode.R))
-        {
-            musicSource.Stop();
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            musicSource.loop = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.L))
-        {
-            musicSource.loop = false;
-        }
     }
 }
